3f20f4dfd4369d63ad7f5d2565112157796d8db7
Tiago Bento <tiagobentofernandes@gmail.com>
2018-08-17 23:29:49 -0300

    appformer-js-bridge module

--- uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/ShowcaseEntryPoint.java
+++ uberfire-showcase/uberfire-webapp/src/main/java/org/uberfire/client/ShowcaseEntryPoint.java
@@ -59,7 +59,7 @@ import org.uberfire.client.perspectives.SimplePerspectiveNoContext;
 import org.uberfire.client.resources.AppResource;
 import org.uberfire.client.screen.JSWorkbenchScreenActivity;
 import org.uberfire.client.screens.popup.SimplePopUp;
-import org.appformer.jsbridge.client.JsWorkbenchScreenActivity;
+import org.uberfire.jsbridge.client.JsWorkbenchScreenActivity;
 import org.uberfire.client.views.pfly.PatternFlyEntryPoint;
 import org.uberfire.client.views.pfly.menu.MainBrand;
 import org.uberfire.client.views.pfly.menu.UserMenu;
