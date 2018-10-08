cfd473dced2b5cbb29c012e976af8fcd15508e71
Caio Uvini <caiouvini@gmail.com>
2018-10-08 18:18:29 -0300

    JS Perspectives initial implementation.
    Current state: perspective being registered at Bean Manager and rendered by AppFormer

--- uberfire-showcase/uberfire-showcase-react-components/src/main/resources/uberfire-showcase-react-components/src/index.tsx
+++ uberfire-showcase/uberfire-showcase-react-components/src/main/resources/uberfire-showcase-react-components/src/index.tsx
@@ -1,9 +1,8 @@
 import {VFSService} from "@kiegroup-ts-generated/uberfire-backend-api-rpc";
-
-alert("I'm alive!");
-
 import * as AppFormer from 'appformer-js';
 import * as React from "react";
+
+alert("I'm alive!");
 
 export class StaticReactComponent extends AppFormer.Screen {
     constructor() {
@@ -27,4 +26,36 @@ export class StaticReactComponent extends AppFormer.Screen {
     }
 }
 
-AppFormer.register({StaticReactComponent});
+export class FirstReactPerspective extends AppFormer.Perspective {
+
+    constructor() {
+        super();
+        this.isReact = true;
+        this.af_isDefault = false;
+        this.af_componentId = "First React Perspective";
+        this.af_perspectiveScreens = [];
+    }
+
+    af_perspectiveRoot(root?: { ss: AppFormer.Screen[]; ps: AppFormer.Perspective[] }): AppFormer.Element {
+        return <div>This is a test perspective!</div>; // TODO create perspective layout here
+    }
+
+    af_onStartup(): void {
+        alert("PERSPECTIVE STARTED!");
+    }
+
+    af_onOpen(): void {
+        alert("PERSPECTIVE OPENED!");
+    }
+
+    af_onClose(): void {
+        alert("PERSPECTIVE CLOSED!");
+    }
+
+    af_onShutdown(): void {
+        alert("PERSPECTIVE SHUTDOWN!");
+    }
+
+}
+
+AppFormer.register({StaticReactComponent, FirstReactPerspective});
