ad199b294c299fc7d7b1e1a832e97a69d08656ba
Tiago Bento <tiagobentofernandes@gmail.com>
2019-05-27 14:54:20 -0300

    Improve code

--- uberfire-security/uberfire-security-client-backend/src/main/java/org/uberfire/security/client/SecurityEntryPoint.java
+++ uberfire-security/uberfire-security-client-backend/src/main/java/org/uberfire/security/client/SecurityEntryPoint.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2019 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,8 +21,6 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import org.jboss.errai.bus.client.ErraiBus;
-import org.jboss.errai.bus.client.util.BusToolsCli;
 import org.jboss.errai.common.client.api.Caller;
 import org.jboss.errai.ioc.client.api.EntryPoint;
 import org.uberfire.backend.authz.AuthorizationService;
